d301bf49203e691565269e4e3b0560090eb118cb
Kodia Steerforth <kodiasteerforth@Kodias-MacBook-Pro.local>
2022-07-28 08:19:05 -0500

    ~/blockchain-developer-bootcamp/.git/COMMIT_EDITMSG

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App';
+import configureStore from './store/configureStore'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
     <App />
-  </React.StrictMode>
+    </Provider>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
